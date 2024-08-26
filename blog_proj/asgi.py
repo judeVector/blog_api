a59e9d0c5cbf3baf02c65070ae9d4aa6452f34d0
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-08-26 16:20:44 +0400

    Add vercel json for production

--- blog_proj/asgi.py
+++ blog_proj/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_proj.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_proj.settings")
 
 application = get_asgi_application()
+
+app = application
