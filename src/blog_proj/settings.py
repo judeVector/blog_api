22692d881812d053de61e6c319488332a2a0fae3
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-09-13 19:35:15 +0400

    Add CORS middleware to pass preflight request issues

--- src/blog_proj/settings.py
+++ src/blog_proj/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
